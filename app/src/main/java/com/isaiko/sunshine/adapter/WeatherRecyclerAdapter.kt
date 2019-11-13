ad31c6f25952f59476052f1dd9365d261ff26ce6
AbdulMuttaleb <a.m.abdelmottaleb@gmail.com>
2019-11-13 14:45:30 +0200

    - forecast recycler adapter - layout for the recycler adapter items

---
+++ app/src/main/java/com/isaiko/sunshine/adapter/WeatherRecyclerAdapter.kt
@@ -0,0 +1,36 @@
+package com.isaiko.sunshine.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.isaiko.sunshine.R
+import java.nio.file.WatchEvent
+
+class WeatherRecyclerAdapter() : RecyclerView.Adapter<WeatherRecyclerAdapter.WeatherViewHolder>(){
+
+    var weatherList:ArrayList<String> = arrayListOf()
+    lateinit var context: Context
+    constructor(context:Context, weatherList:ArrayList<String>):this(){
+        this.context = context
+        this.weatherList = weatherList
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeatherViewHolder {
+        return WeatherViewHolder(LayoutInflater.from(context).inflate(R.layout.item_forecast,parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return weatherList.size
+    }
+
+    override fun onBindViewHolder(holder: WeatherViewHolder, position: Int) {
+        val weather = weatherList[position]
+        holder.weatherTextView.text = weather
+    }
+
+    inner class WeatherViewHolder(itemView:View): RecyclerView.ViewHolder(itemView){
+        var weatherTextView: TextView = itemView.findViewById(R.id.tv_text)
+    }
+}
--- app/src/main/java/com/isaiko/sunshine/view/MainActivity.kt
+++ app/src/main/java/com/isaiko/sunshine/view/MainActivity.kt
@@ -13,47 +13,53 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.isaiko.sunshine
+package com.isaiko.sunshine.view
 
-import android.content.Context
 import android.os.AsyncTask
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 import com.isaiko.sunshine.utilities.NetworkUtils
 import com.isaiko.sunshine.utilities.OpenWeatherJsonUtils
 import java.lang.Exception
 import com.isaiko.sunshine.data.SunshinePreferences
-import android.support.v4.app.SupportActivity
-import android.support.v4.app.SupportActivity.ExtraData
-import android.support.v4.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.isaiko.sunshine.R
+import com.isaiko.sunshine.adapter.WeatherRecyclerAdapter
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var mWeatherTextView: TextView
     lateinit var errorTextView: TextView
     lateinit var progressBar: ProgressBar
+
+    lateinit var weatherRecyclerAdapter:WeatherRecyclerAdapter
+    lateinit var weatherRecyclerView:RecyclerView
+
+    var weatherList:ArrayList<String> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forecast)
 
-        mWeatherTextView = findViewById(R.id.tv_weather_data)
         errorTextView = findViewById(R.id.tv_loading_error)
         progressBar = findViewById(R.id.pb_main)
+        weatherRecyclerView = findViewById(R.id.rv_weather)
+
+        weatherRecyclerAdapter = WeatherRecyclerAdapter(this, weatherList)
+        weatherRecyclerView.layoutManager = LinearLayoutManager(this)
+        weatherRecyclerView.adapter = weatherRecyclerAdapter
 
         loadWeatherData()
     }
 
     fun loadWeatherData(){
         val location = SunshinePreferences.getPreferredWeatherLocation(this)
-        mWeatherTextView.text = ""
         FetchWeatherAsyncTask().execute(location)
     }
 
@@ -87,9 +93,11 @@ class MainActivity : AppCompatActivity() {
         override fun onPostExecute(result: ArrayList<String>?) {
             if(result!=null){
                 for(weather in result){
-                    errorTextView.visibility = View.GONE
-                    mWeatherTextView.append((weather) + "\n\n\n");
                 }
+                weatherList.clear()
+                weatherList.addAll(result)
+                weatherRecyclerAdapter.notifyDataSetChanged()
+                errorTextView.visibility = View.GONE
             }
 
             progressBar.visibility = View.GONE
