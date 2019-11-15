30560fb413907140990324186efa06a082fdbc00
AbdulMuttaleb <a.m.abdelmottaleb@gmail.com>
2019-11-15 12:10:05 +0200

    - share weather data

--- app/src/main/java/com/isaiko/sunshine/view/DetailActivity.kt
+++ app/src/main/java/com/isaiko/sunshine/view/DetailActivity.kt
@@ -1,15 +1,18 @@
 package com.isaiko.sunshine.view
 
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ShareCompat
 import com.isaiko.sunshine.R
 
 class DetailActivity : AppCompatActivity(){
 
     lateinit var detailsTextView:TextView
-
+    var passedWeatherDetail:String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
@@ -17,7 +20,7 @@ class DetailActivity : AppCompatActivity(){
 
         detailsTextView = findViewById(R.id.tv_weather_detail)
 
-        val passedWeatherDetail:String? = intent.getStringExtra("text")
+         passedWeatherDetail = intent.getStringExtra("text")
 
         if(!passedWeatherDetail.isNullOrEmpty()){
             detailsTextView.text = passedWeatherDetail
@@ -31,7 +34,36 @@ class DetailActivity : AppCompatActivity(){
                 finish()
                 true
             }
+
+            R.id.action_share ->{
+                if(!passedWeatherDetail.isNullOrEmpty()){
+                    shareText(passedWeatherDetail!!)
+                }else{
+                    Toast.makeText(this, "Invalid data",Toast.LENGTH_SHORT).show()
+                }
+                true
+            }
             else -> super.onOptionsItemSelected(item);
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.details, menu)
+        return true
+    }
+
+    fun shareText(text:String){
+        val mimeType = "text/plain"
+        val title = "Share"
+
+        val shareIntent = ShareCompat.IntentBuilder.from(this)
+                .setType(mimeType)
+                .setChooserTitle(title)
+                .setText(text)
+                .intent
+
+        if(shareIntent.resolveActivity(packageManager) != null){
+            startActivity(shareIntent)
         }
     }
 }
